d7ee7cfed2fdd4910f2975155d806f2b475e1a7d
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-05-09 11:02:36 +0300

    replicate

---
+++ create.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# See: https://www.percona.com/blog/2016/03/30/docker-mysql-replication-101/
+
+function wait_mysql {
+    container_name=$1;
+    docker exec -it $container_name mysql -Nsqe "use replicated_db;" >/dev/null 2>/dev/null;
+    if [ $? -ne 0 ]
+    then
+        echo -n .
+        sleep 1
+        wait_mysql $container_name
+    fi
+}
+
+## Run containers
+docker-compose up -d
+
+## Wait for master
+echo -n Waiting for container rpl_mysql_master
+wait_mysql "rpl_mysql_master";
+echo ''
+
+## Wait for slave
+echo -n Waiting for container rpl_mysql_slave
+wait_mysql "rpl_mysql_slave";
+echo ''
+
+## Run on MASTER
+
+# allow access of slave user
+echo 'Add permissions for slave on master'
+docker exec -it rpl_mysql_master mysql -Nsqe "GRANT REPLICATION SLAVE ON *.* TO 'slave'@'%' IDENTIFIED BY 'slave'; FLUSH PRIVILEGES;"
+
+# Lock table
+echo 'Lock tables on master'
+docker exec -it rpl_mysql_master mysql -Nsqe "USE replicated_db; FLUSH TABLES WITH READ LOCK;"
+
+# Get file and position
+echo 'Get file and position of replication'
+MASTER_LOG_FILE=$(docker exec -it rpl_mysql_master mysql -Nsqe "SHOW MASTER STATUS \G" | sed '2q;d')
+MASTER_LOG_POS=$(docker exec -it rpl_mysql_master mysql -Nsqe "SHOW MASTER STATUS \G" | sed '3q;d')
+
+# Dump current master
+echo 'Dump current master database'
+docker exec -it rpl_mysql_master bash -c 'mysqldump -u root replicated_db > /var/mysql/common/replicated_db.sql'
+
+# Unlock
+echo 'Unlock master tables'
+docker exec -it rpl_mysql_master mysql -Nsqe "USE replicated_db; UNLOCK TABLES;"
+
+## Run on SLAVE
+docker exec -it rpl_mysql_slave bash -c 'mysql replicated_db < /var/mysql/common/replicated_db.sql'
+
+docker exec -it rpl_mysql_slave mysql -Nsqe "CHANGE MASTER TO MASTER_HOST='mysql_master', MASTER_USER='slave', MASTER_PASSWORD='slave', MASTER_LOG_FILE = '$MASTER_LOG_FILE', MASTER_LOG_POS = $MASTER_LOG_POS;"
+docker exec -it rpl_mysql_slave mysql -Nsqe "START SLAVE;"
+
+## Show slave status
+docker exec -it rpl_mysql_slave mysql -sqe "SHOW SLAVE STATUS\G"
+
---
+++ remove.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ask for confirm
+while true; do
+    read -p "Do you really want to drop containers? (y/n): " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# drop shared data
+sudo rm -rf ./shared
+
+# stop containers
+docker ps -a -f NAME=rpl_mysql --format "{{.Names}}" | xargs -I{} docker stop {}
+
+# drop containers
+docker ps -a -f NAME=rpl_mysql --format "{{.Names}}" | xargs -I{} docker rm {}
